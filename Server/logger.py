7018a69f90a90e9ba781dde2fc3ff71bcf249378
Owain Jones <tehdoomcat@gmail.com>
2012-06-27 23:58:06 +0100

    First commit

---
+++ Server/logger.py
@@ -0,0 +1,15 @@
+'''
+@author: Owain Jones [odj@aber.ac.uk]
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ Server/main.py
@@ -0,0 +1,6 @@
+'''
+@author: Owain Jones [odj@aber.ac.uk]
+'''
+
+if __name__ == '__main__':
+    print "Yes, this is dog"
